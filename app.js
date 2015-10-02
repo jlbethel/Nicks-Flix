8032695eede055555aad635282e0b4d4eb021e10
Jason Bethel <jlbethel@gmail.com>
2015-10-02 09:04:49 -0700

    add routes to body and header in app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+var nicksFlix = angular.module('nicksFlix', ['ui.router']);
+
+nicksFlix.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('home', {
+    url: "",
+    views: {
+      'header': {
+        templateUrl: "partials/header.html",
+      },
+      'body': {
+        templateUrl: "partials/home.html",
+      },
+    }
+  });
+});
