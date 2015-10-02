cc2a67dc96c04fb0d267284f57d5a8dfdf4f44bb
Jason Bethel <jlbethel@gmail.com>
2015-10-02 14:43:48 -0700

    add comments to code

--- directives/myDirectives.js
+++ directives/myDirectives.js
@@ -1,6 +1,6 @@
+//Night Mode directive. Toggles filter: invert
 nicksFlix.directive("nightMode", function() {
   return function (scope, element, attrs) {
-
   element.bind("click", function() {
     var page = angular.element(document).find('body');
       page.toggleClass("night");
--- services/UtilitiesFactory.js
+++ services/UtilitiesFactory.js
@@ -1,5 +1,7 @@
 nicksFlix.factory('UtilitiesFactory', function() {
   return {
+
+    //function to find reviews by title
     findByTitle: function(collection, title) {
       for (var i = 0; i < collection.length; i++) {
         if (collection[i].title == title)
